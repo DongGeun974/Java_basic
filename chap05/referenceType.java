e535cc115cdd4efe16b0fdaf5081b7996711a1be
DongGeun974 <zxcasd3004@naver.com>
2022-05-28 18:01:20 +0900

    220528 hdg

--- chap05/referenceType.java
+++ chap05/referenceType.java
@@ -22,5 +22,39 @@ public class referenceType {
         System.out.println(name3 != name4);
 
         System.out.println(name3);
+
+        System.out.println("-----------------------------------------------");
+
+        int[] intArr;
+        String[] strArr;
+
+        int arrInt[];
+        String arrStr[];
+
+        String name[] = {"test"};
+
+        System.out.println(name[0]);
+
+//        intArr = {1, 2};
+        intArr = new int[] {1, 2};
+        System.out.println(intArr[0]);
+
+        int scores[][] = new int[2][3];
+        System.out.println(scores.length);
+        System.out.println(scores[0].length);
+
+        int s[][] = {{1,2}, {3,4}};
+
+        for (int score : s[0])
+        {
+            System.out.println(score);
+        }
+
+        System.out.println("-----------------------------------------------");
+
+        Week today = Week.Mon;
+        System.out.println(today.getClass().getName());
     }
+
+    public enum Week {Mon, Tue, Wed, Thu, Fri}
 }
